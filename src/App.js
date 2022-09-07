641d9dc1fdc659edbd7c285aa67ff80ad3cfaf28
MrBellon <104333083+MrBellon@users.noreply.github.com>
2022-09-07 13:49:00 +0900

    feat : test 중입니다

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import RouteTest from './components/RouteTest';
+import RouteTest from "./components/RouteTest";
 
-import Home from './pages/Home';
-import New from './pages/New';
-import Edit from './pages/Edit';
-import Diary from './pages/Diary';
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
 function App() {
   return (
@@ -14,10 +14,10 @@ function App() {
       <div className="App">
         <h2>App.js</h2>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/new' element={<New />} />
-          <Route path='/edit' element={<Edit />} />
-          <Route path='/diary/:id' element={<Diary />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
         </Routes>
         <RouteTest />
       </div>
